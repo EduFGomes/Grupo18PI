85d7630e993b5ca4c3b669895c6bf1393e530876
Joaopzf <162457155+Joaopzf@users.noreply.github.com>
2024-03-26 21:30:49 -0300

    Update Fase1.py
    Adicionei a informação final sobre a rentabilidade (alta, média, baixa...) e também aproveitei pra formatar a tabela, deixei só duas casas decimais aparecendo nos números e porcentagens e adicionei  um 'if' testando se o preço de venda é menor ou igual a zero, para fechar o programa direto caso o preço de venda fique gratuito ou negativo.
    
    Não sei se foi o melhor método de formatação então se quiserem testar e melhorar fiquem a vontade. Vou dar commit na main porque não apaguei nada do programa original então não vejo problema.

--- Fase1.py
+++ Fase1.py
@@ -11,13 +11,36 @@ pv = cp / (1 - ((cf + cv + iv + ml)/100))
 lucro_real = ml
 lucro_pcpv = pv / cp - 100
 
-print(f"Descrição                      Valor  %")
-print(f"A. Preço de venda              {pv}    100%")
-print(f"B. Custo de aquisição          {cp}    {(cp * 100) / pv}%")
-print(f"C. Receita Bruta               {pv - cp}    {100 - ((cp * 100) / pv)}%")
-print(f"D. Custo fixo/Administrativo   {(pv * cf) / 100}    {cf}%")
-print(f"D. Custo fixo/administrativo   {cf}    {(cf * 100) / pv}%")
-print(f"E. Comissão de vendas          {cv}    {(cv * 100) / pv}%")
-print(f"F. Impostos                    {iv}    {(iv * 100) / pv}%")
-print(f"G. Outros custos               {cf + cv + iv}    {((cf + cv + iv) * 100) / pv}%")
-print(f"H. Rentabilidade               {(pv - cp) - (cf + cv + iv)}    {(cv * 100) / pv}%")
+if(pv<=0):
+    print('-'*45)
+    print(f'Preço de venda menor ou igual a zero -> {pv}')
+else:
+    print('-'*45)
+    print(f"Descrição                      {'Valor':6} | {'  %':6}")
+    print(f"A. Preço de venda              {pv:6.2f} | {'   100%':6}")
+    print(f"B. Custo de aquisição          {cp:6.2f} | {(cp * 100) / pv:6.2f}%")
+    print(f"C. Receita Bruta               {pv - cp:6.2f} | {100 - ((cp * 100) / pv):6.2f}%")
+    print(f"D. Custo fixo/Administrativo   {(pv * cf) / 100:6.2f} | {cf:6.2f}%")
+    print(f"D. Custo fixo/administrativo   {cf:6.2f} | {(cf * 100) / pv:6.2f}%")
+    print(f"E. Comissão de vendas          {cv:6.2f} | {(cv * 100) / pv:6.2f}%")
+    print(f"F. Impostos                    {iv:6.2f} | {(iv * 100) / pv:6.2f}%")
+    print(f"G. Outros custos               {cf + cv + iv:6.2f} | {((cf + cv + iv) * 100) / pv:6.2f}%")
+    print(f"H. Rentabilidade               {(pv - cp) - (cf + cv + iv):6.2f} | {(cv * 100) / pv:6.2f}%")
+    
+    if(((cv * 100) / pv)>20):
+        print('-'*45)
+        print('Lucro alto')
+    elif(10<((cv * 100) / pv)<=20):
+        print('-'*45)
+        print('Lucro médio')
+    elif(0<((cv * 100) / pv)<=10):
+        print('-'*45)
+        print('Lucro baixo')
+    elif(((cv * 100) / pv)==0):
+        print('-'*45)
+        print('Equilíbrio')
+    elif(((cv * 100) / pv)<0):
+        print('-'*45)
+        print('Prejuízo')
+print('-'*45)
+    
