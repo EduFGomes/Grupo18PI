e83334bcf0fb892ee4467765e065db7e05f5af89
Joaopzf <162457155+Joaopzf@users.noreply.github.com>
2024-04-18 13:28:52 -0300

    Update Fase1.py

--- Fase1.py
+++ Fase1.py
@@ -17,7 +17,7 @@ if(pv<=0):
 else:
     print('-'*45)
     print(f"Descrição                      {'Valor':6} | {'  %':6}")
-    print(f"A. Preço de venda              {pv:6.2f} | {'   100%':6}")
+    print(f"A. Preço de venda              {pv:6.2f} | {'100.00%':6}")
     print(f"B. Custo de aquisição          {cp:6.2f} | {(cp * 100) / pv:6.2f}%")
     print(f"C. Receita Bruta               {pv - cp:6.2f} | {100 - ((cp * 100) / pv):6.2f}%")
     print(f"D. Custo fixo/Administrativo   {(pv * cf) / 100:6.2f} | {cf:6.2f}%")
@@ -27,20 +27,19 @@ else:
     print(f"G. Outros custos               {cf + cv + iv:6.2f} | {((cf + cv + iv) * 100) / pv:6.2f}%")
     print(f"H. Rentabilidade               {(pv - cp) - (cf + cv + iv):6.2f} | {(cv * 100) / pv:6.2f}%")
     
-    if(((cv * 100) / pv)>20):
+    if(ml>20):
         print('-'*45)
         print('Lucro alto')
-    elif(10<((cv * 100) / pv)<=20):
+    elif(10<ml<=20):
         print('-'*45)
         print('Lucro médio')
-    elif(0<((cv * 100) / pv)<=10):
+    elif(0<ml<=10):
         print('-'*45)
         print('Lucro baixo')
-    elif(((cv * 100) / pv)==0):
+    elif(ml==0):
         print('-'*45)
         print('Equilíbrio')
-    elif(((cv * 100) / pv)<0):
+    elif(ml<0):
         print('-'*45)
         print('Prejuízo')
 print('-'*45)
-    
