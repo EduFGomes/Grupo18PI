f1bd0e62b2d7e927752ff16e2caa65c4d7c53762
Joaopzf <162457155+Joaopzf@users.noreply.github.com>
2024-05-14 09:37:24 -0300

    Update Fase3Inserir.py
    Coloquei uma função para fazer a inserção e coloquei o código inteiro em loop para poder inserir múltiplos itens de uma vez só.
    
    Abri uma branch com as mudanças, pois acho que não está bem feito e ainda dá para otimizar antes de definir como a main.

--- Fase3Inserir.py
+++ Fase3Inserir.py
@@ -1,36 +1,43 @@
 import getpass
 import oracledb
 
-try:
-    conexao = oracledb.connect(
-    user = "BD150224132",
-    password = "Crcud6",
-    dsn = 'BD-ACD/xe')
-except Exception as erro:
-    print("Erro ao conectar", erro)
-else:
-    print("Conectado", conexao.version)
-    cursor = conexao.cursor()
-    
-    cursor.execute("""select distinct codigo_prod from tabela_produtos""")
-    tabela = cursor.fetchall()
+def inserir(cod, Inome, Idescricao, Icp, Icf, Icv, Iiv, Iml):
+    cursor.execute(f"""insert into tabela_produtos (codigo_prod, nome, descricao, custo_prod, custo_fixo, comissao_venda, impostos, rentabilidade)
+    values ({cod}, '{Inome}', '{Idescricao}', {Icp}, {Icf}, {Icv}, {Iiv}, {Iml})""")
 
-    cod = (len(tabela) + 1)
-    Inome = input("Nome do produto: ")
-    Idescricao = input("Descrição: ")
-    Icp = float(input("Custo do produto: "))
-    Icf = int(input("Custo fixo: "))
-    Icv = int(input("Comissão de venda: "))
-    Iiv = int(input("Impostos: "))
-    Iml = int(input("Margem de lucro: "))
-
-    if(Icf + Icv + Iiv + Iml) < 100:
-        cursor.execute(f"""insert into tabela_produtos (codigo_prod, nome, descricao, custo_prod, custo_fixo, comissao_venda, impostos, rentabilidade)
-        values ({cod}, '{Inome}', '{Idescricao}', {Icp}, {Icf}, {Icv}, {Iiv}, {Iml})""")
+quant = int(input('Quantos itens deseja inserir? '))
+for i in range(quant):
+    try:
+        conexao = oracledb.connect(
+        user = "BD150224325",
+        password = "Rpotd1",
+        dsn = 'BD-ACD/xe')
+    except Exception as erro:
+        print("Erro ao conectar", erro)
     else:
-        print("Não é possível inserir na tabela, pois a soma das porcentagens não pode ser maior ou igual a 100%")
+        print("Conectado", conexao.version)
+        cursor = conexao.cursor()
+    
+        cursor.execute("""select distinct codigo_prod from tabela_produtos""")
+        tabela = cursor.fetchall()
 
-    conexao.commit()
+        print(f'{i+1}° Item: ')
+        cod = (len(tabela) + 1)
+        Inome = input("Nome do produto: ")
+        Idescricao = input("Descrição: ")
+        Icp = float(input("Custo do produto: "))
+        Icf = int(input("Custo fixo: "))
+        Icv = int(input("Comissão de venda: "))
+        Iiv = int(input("Impostos: "))
+        Iml = int(input("Margem de lucro: "))
 
-    cursor.close
-    conexao.close
+        if(Icf + Icv + Iiv + Iml) < 100:
+            inserir(cod, Inome, Idescricao, Icp, Icf, Icv, Iiv, Iml)
+            print('Item inserido com sucesso!')
+        else:
+            print("Não é possível inserir na tabela, pois a soma das porcentagens não pode ser maior ou igual a 100%")
+    
+        conexao.commit()
+
+        cursor.close
+        conexao.close
