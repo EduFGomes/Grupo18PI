ee1bacfa1c8fd41d1f177eac411f3f331c58e683
pineapplech <164060637+pineapplech@users.noreply.github.com>
2024-05-30 15:58:00 -0300

    Update Fase3Criptografia.py

--- Fase3Criptografia.py
+++ Fase3Criptografia.py
@@ -7,8 +7,7 @@ def multip(matriz, pares):
         (matriz[1][0] * pares[0] + matriz[1][1] * pares[1]) % 26
     ]
 
-
-def criptografia(cifra_hill, texto, chave):
+def criptografia(texto, chave):
     texto = texto.replace(" ", "").lower()
     if len(texto) % 2 != 0:
         texto += texto[-1]
@@ -24,6 +23,22 @@ def criptografia(cifra_hill, texto, chave):
     
     return cripto
 
+chaveinv = [[42, -63],[-21, 84]]
+def descriptografia(cripto): 
+    pares = []
+    for i in range(0, len(cripto), 2):
+        p1 = cifra_hill.index(cripto[i])
+        p2 = cifra_hill.index(cripto[i+1])
+        pares.append([p1, p2])
+    descripto = ""
+    for par in pares:
+        b = multip(chaveinv, par)
+        descripto += cifra_hill[b[0]] + cifra_hill[b[1]]
+    return descripto
+
 texto = input("insira a mensagem a ser criptografada:")
 textocripto = criptografia(texto, chave)
 print('texto criptografado:', textocripto)
+textodescripto = descriptografia(textocripto)
+print('texto descriptografado:', textodescripto)
+
