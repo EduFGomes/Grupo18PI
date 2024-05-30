63f4f2c19b7c4032363f4bf3c5d640505e068fed
pineapplech <164060637+pineapplech@users.noreply.github.com>
2024-05-30 15:37:47 -0300

    Update Fase3Descriptografia.py

--- Fase3Descriptografia.py
+++ Fase3Descriptografia.py
@@ -1,10 +1,12 @@
-def descriptografia(cripto): 
+cifra_hill = "abcdefghijklmnopqrstuvwxyz"
+chaveinv = [[42, -63],[-21, 84]]
+
+def descriptografia(cripto, cifra_hill, chaveinv): 
     pares = []
     for i in range(0, len(cripto), 2):
         p1 = cifra_hill.index(cripto[i])
         p2 = cifra_hill.index(cripto[i+1])
         pares.append([p1, p2])
-    chaveinv = [[42, -63],[-21, 84]]
     descripto = ""
     for par in pares:
         b = multip(chaveinv, par)
