dedd8d1a7bd4b2f613f350c8896a225052e21780
Joaopzf <162457155+Joaopzf@users.noreply.github.com>
2024-05-30 19:00:33 -0300

    Update Fase3.py

--- Fase3.py
+++ Fase3.py
@@ -1,46 +1,43 @@
 import getpass
 import oracledb
 
-def conexao():
-    try:
-        conexao = oracledb.connect(
-        user = "BD150224132",
-        password = "Crcud6",
-        dsn = '172.16.12.14/xe')
-    except Exception as erro:
-        print("Erro ao conectar", erro)
-    else:
-        print("Conectado", conexao.version)
-        cursor = conexao.cursor()
+def multip(matriz, pares):
+    return [
+        (matriz[0][0] * pares[0] + matriz[0][1] * pares[1]) % 26,
+        (matriz[1][0] * pares[0] + matriz[1][1] * pares[1]) % 26
+    ]
+
+def criptografia(texto, chave):
+    texto = texto.replace(" ", "").lower()
+    if len(texto) % 2 != 0:
+        texto += texto[-1]
+    pares = []
+    for i in range(0, len(texto), 2):
+        p1 = cifra_hill.index(texto[i])
+        p2 = cifra_hill.index(texto[i+1])
+        pares.append([p1, p2])
+    cripto = ""
+    for par in pares:
+        a = multip(chave, par)
+        cripto += cifra_hill[a[0]] + cifra_hill[a[1]]
+    
+    return cripto
+
+def descriptografia(cripto): 
+    pares = []
+    for i in range(0, len(cripto), 2):
+        p1 = cifra_hill.index(cripto[i])
+        p2 = cifra_hill.index(cripto[i+1])
+        pares.append([p1, p2])
+    descripto = ""
+    for par in pares:
+        b = multip(chaveinv, par)
+        descripto += cifra_hill[b[0]] + cifra_hill[b[1]]
+    return descripto
 
 def inserir(cod, Inome, Idescricao, Icp, Icf, Icv, Iiv, Iml):
     cursor.execute(f"""insert into tabela_produtos (codigo_prod, nome, descricao, custo_prod, custo_fixo, comissao_venda, impostos, rentabilidade)
     values ({cod}, '{Inome}', '{Idescricao}', {Icp}, {Icf}, {Icv}, {Iiv}, {Iml})""")
-
-
-    cursor.execute("""select distinct codigo_prod from tabela_produtos""")
-    tabela = cursor.fetchall()
-
-    quant = int(input('Quantos itens deseja inserir? '))
-    for i in range(quant):
-
-        print(f'{i+1}° Item: ')
-        cod = (len(tabela) + 1)
-        Inome = input("Nome do produto: ")
-        Idescricao = input("Descrição: ")
-        Icp = float(input("Custo do produto: "))
-        Icf = int(input("Custo fixo: "))
-        Icv = int(input("Comissão de venda: "))
-        Iiv = int(input("Impostos: "))
-        Iml = int(input("Margem de lucro: "))
-
-        if(Icf + Icv + Iiv + Iml) < 100:
-            inserir(cod, Inome, Idescricao, Icp, Icf, Icv, Iiv, Iml)
-            print('Item inserido com sucesso!')
-        else:
-            print("Não é possível inserir na tabela, pois a soma das porcentagens não pode ser maior ou igual a 100%")
-
-        conexao.commit()
        
 def apagar():
     def apagar_produto_por_nome(cursor, nome_produto):
@@ -53,7 +50,7 @@ def apagar():
     def apagar_produto_por_codigo(cursor, codigo_produto):
         try:
             cursor.execute("SELECT nome FROM tabela_produtos WHERE codigo_prod = :codigo_produto", {'codigo_produto': codigo_produto})
-                nome_produto = cursor.fetchone()[0]
+            nome_produto = cursor.fetchone()[0]
             confirmacao = input("Tem certeza que deseja remover o produto '{}' com o código {}? (Sim/Não): ".format(nome_produto, codigo_produto)).lower()
             if confirmacao == "sim":
                 cursor.execute("DELETE FROM tabela_produtos WHERE codigo_prod = :codigo_produto", {'codigo_produto': codigo_produto})
@@ -103,6 +100,67 @@ def apagar():
         
     conexao.commit()
 
+def alterar():   
+    def tela_alteracao():
+        print ('\n', "-"*90, '\n')
+        print (" "*25 , 'BEM VINDO AO MENU DE ALTERAÇÃO')
+        print ('\n', "-"*90, '\n')
+        
+        
+    def navegarAlteracoes(tela_atualA):
+        while True:
+            telas[tela_atualA]()
+            resposta = str (input ('Deseja alterar algo? \n[SIM] ou [NAO]\n'))
+            if resposta.lower() == 'sim':
+                print ('Para alterar valores de uma coluna específica - digite "1"')
+                print ('Para alterar o nome de uma coluna específica - digite "2"')
+                escolha = int(input('digite o número desejado: '))
+                if escolha == 1:
+                    tela_atualA = 'Avalores'
+                elif escolha == 2:
+                    tela_atualA = 'Anome'
+                else:
+                    print ('tela não reconhecida, tente novamente')
+            if resposta.lower() == 'nao':
+                print ('voltando ao menu')
+                
+    def tela_Avalores():
+        print("TELA DE ALTERAÇÃO DE VALORES")
+        Avalor_antigo = str (input ('Nome antigo (igual escrito na tabela): '))
+        Avalor_novo = str (input ('Nome novo: '))
+        Acoluna = str (input ('Nome da coluna: '))
+        cursor.execute (f"""
+                        update tabela_produtos 
+                        set {Acoluna} = '{Avalor_novo}'
+                        where {Acoluna} = '{Avalor_antigo}'
+                        """)                     
+        conexao.commit()
+        print ('Valor atualizado com sucesso!')
+
+    def tela_Anome():
+        print("TELA DE ALTERAÇÃO DE NOME")
+        Anome_antigo = str (input ('Nome antigo da coluna(igual escrito na tabela): '))
+        Anome_novo = str (input ('Nome novo da coluna: '))
+        cursor.execute (f"""
+                        ALTER TABLE tabela_produtos 
+                        RENAME COLUMN {Anome_antigo} to {Anome_novo} 
+                        """)
+        conexao.commit()
+        print ('Nome da coluna atualizado com sucesso!')
+    
+
+    telas = {'menu alteracao' : tela_alteracao,
+            'Avalores': tela_Avalores,
+            'Anome': tela_Anome,
+            'navegar alteracoes':navegarAlteracoes
+            }
+
+    def mainAlteracoes():
+        tela_atualA = 'menu alteracao'
+        navegarAlteracoes (tela_atualA)
+
+    mainAlteracoes()
+
 def listar():
     cursor.execute("""select distinct codigo_prod from tabela_produtos""")
     tabela = cursor.fetchall()
@@ -130,8 +188,8 @@ def listar():
         else:
             print('-'*45)
             print("Código do produto:", cod)
-            print("Nome do produto:", nome)
-            print()
+            print("Nome do produto:", nome[0][0])
+            print(f"Descrição o produto: {descriptografia(Idescricao)}\n")
             print(f"Descrição                      {'Valor':6} | {'  %':6}")
             print(f"A. Preço de venda              {pv:6.2f} | {'   100%':6}")
             print(f"B. Custo de aquisição          {cp[0][0]:6.2f} | {(cp[0][0] * 100) / pv:6.2f}%")
@@ -159,5 +217,56 @@ def listar():
                 print('Prejuízo')
                 print('-'*45)
 
-connection.close()
+try:
+    conexao = oracledb.connect(
+    user = "TESTE",
+    password = "teste",
+    dsn = 'localhost/XEPDB1')
+except Exception as erro:
+    print("Erro ao conectar", erro)
+else:
+    print("Conectado", conexao.version)
+    cursor = conexao.cursor()
+
+cifra_hill = "abcdefghijklmnopqrstuvwxyz"
+chave = [[4, 3], [1, 2]]
+chaveinv = [[42, -63],[-21, 84]]
+
+escolha = 1
+while escolha != 0:
+    escolha = int(input('O que deseja fazer?\n[1] Inserir Produtos\n[2] Remover Produtos\n[3] Alterar Produtos\n[4] Listar Produtos\n[0] Sair\n:'))
+    if escolha == 1:
+        cursor.execute("""select distinct codigo_prod from tabela_produtos""")
+        tabela = cursor.fetchall()
+        quant = int(input('Quantos itens deseja inserir? '))
+        for i in range(quant):
+            print(f'{i+1}° Item: ')
+            cod = (len(tabela) + 1)
+            Inome = input("Nome do produto: ")
+            Idescricao = input("Descrição: ")
+            Idescricao = criptografia(Idescricao, chave)
+            Icp = float(input("Custo do produto: "))
+            Icf = int(input("Custo fixo: "))
+            Icv = int(input("Comissão de venda: "))
+            Iiv = int(input("Impostos: "))
+            Iml = int(input("Margem de lucro: "))
+            if(Icf + Icv + Iiv + Iml) < 100:
+                inserir(cod, Inome, Idescricao, Icp, Icf, Icv, Iiv, Iml)
+                print('Item inserido com sucesso!')
+            else:
+                print("Não é possível inserir na tabela, pois a soma das porcentagens não pode ser maior ou igual a 100%")
+            conexao.commit()
+    elif escolha == 2:
+        apagar()
+    elif escolha == 3:
+        alterar()
+    elif escolha == 4:
+        listar()
+    elif escolha == 0:
+        print('Saindo...')
+    else:
+        print('Opção inválida! Por favor, escolha uma opção válida.')
+print('Fim do Programa.')
+
 cursor.close()
+conexao.close()
